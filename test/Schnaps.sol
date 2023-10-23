ac34203078925729f26e8c2aab623063b098d50e
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-10-23 16:42:30 +0200

    Add withdrawal tests

--- test/Schnaps.sol
+++ test/Schnaps.sol
@@ -11,10 +11,12 @@ contract SchnapsTest is Test {
     address owner = address(0xbeef);
     uint256 amount = 100;
     uint256 supply = 1000;
+    address payable receiver = payable(address(1337));
     bytes barcode = "0xdeadbeef";
     IERC20 token;
 
     event PaymentReceived(address sender, address token, uint256 amount, bytes barcode);
+    event Withdrawal(address receiver, address token, uint256 amount);
 
     function setUp() public {
         schnaps = new Schnaps(owner);
@@ -33,5 +35,53 @@ contract SchnapsTest is Test {
         vm.expectEmit(true, true, true, true);
         emit PaymentReceived(address(this), address(token), amount, barcode);
         schnaps.payWithToken(token, amount, barcode);
+    }
+
+    function test_withdrawEth() public {
+        schnaps.payWithEth{value: amount}(barcode);
+
+        vm.prank(owner);
+        vm.expectEmit(true, true, true, true);
+        emit Withdrawal(receiver, address(0), amount);
+        schnaps.withdrawEth(receiver, amount);
+    }
+
+    function test_withdrawEthUnauthorized() public {
+        schnaps.payWithEth{value: amount}(barcode);
+
+        vm.expectRevert();
+        schnaps.withdrawEth(receiver, amount);
+    }
+
+    function test_withdrawEthTooMuch() public {
+        schnaps.payWithEth{value: amount}(barcode);
+
+        vm.prank(owner);
+        vm.expectRevert();
+        schnaps.withdrawEth(receiver, amount + 1);
+    }
+
+    function test_withdrawToken() public {
+        schnaps.payWithToken(token, amount, barcode);
+
+        vm.prank(owner);
+        vm.expectEmit(true, true, true, true);
+        emit Withdrawal(receiver, address(token), amount);
+        schnaps.withdrawToken(token, receiver, amount);
+    }
+
+    function test_withdrawTokenUnauthorized() public {
+        schnaps.payWithToken(token, amount, barcode);
+
+        vm.expectRevert();
+        schnaps.withdrawToken(token, receiver, amount);
+    }
+
+    function test_withdrawTokenTooMuch() public {
+        schnaps.payWithToken(token, amount, barcode);
+
+        vm.prank(owner);
+        vm.expectRevert();
+        schnaps.withdrawToken(token, receiver, amount + 1);
     }
 }
