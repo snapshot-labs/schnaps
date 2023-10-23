44a2622e01e592ea5600abab596e2d65df5edab3
Orlando <orlando@snapshot.org>
2023-10-23 16:32:06 +0100

    chore: CREATE3 deployer script

---
+++ script/Deployer.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import "forge-std/Script.sol";
+import "../src/Schnaps.sol";
+import "../lib/create3-factory/src/ICREATE3Factory.sol";
+
+contract Deployer is Script {
+    using stdJson for string;
+
+    string internal deployments;
+    string internal deploymentsPath;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        string memory network = vm.envString("NETWORK");
+        address owner = vm.envAddress("OWNER");
+
+        deploymentsPath = string.concat(string.concat("./deployments/", network), ".json");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Using the CREATE3 factory maintained by lififinance: https://github.com/lifinance/create3-factory
+        address deployed = ICREATE3Factory(0x93FEC2C00BfE902F733B57c5a6CeeD7CD1384AE1).deploy(
+            bytes32(uint256(0)), abi.encodePacked(type(Schnaps).creationCode, abi.encode(owner))
+        );
+
+        deployments = deployments.serialize("Schnaps", deployed);
+
+        deployments.write(deploymentsPath);
+
+        vm.stopBroadcast();
+    }
+}
