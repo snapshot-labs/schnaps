7a28455fe108cc9e9695baa490e5ea00dfc40041
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-10-23 15:08:48 +0200

    init commit

---
+++ src/Schnaps.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Schnaps {
+    // create depositEth function
+    // anyone can call depositEth function, payable
+    // should have calldata
+    // should emit an event with (sender, calldata, amount) (indexed?)
+
+    // create depositToken function
+    // 
+
+    // create withdrawEth
+    // onlyOwner : takes an address
+    // create withdrawToken
+    // onlyOwner : takes an address
+
+}
