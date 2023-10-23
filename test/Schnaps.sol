ea74356fff1f1eced706fd01ec60637301d4d639
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-10-23 15:52:53 +0200

    add basic tests

--- test/Schnaps.sol
+++ test/Schnaps.sol
@@ -1,24 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-// import {Test, console2} from "forge-std/Test.sol";
-// import {Counter} from "../src/Counter.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {Schnaps} from "../src/Schnaps.sol";
+import { TestToken } from "./mock.sol";
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 
-// contract CounterTest is Test {
-//     Counter public counter;
+contract SchnapsTest is Test {
+    Schnaps public schnaps;
+    address owner = address(0xbeef);
+    uint256 amount = 100;
+    uint256 supply = 1000;
+    bytes barcode = "0xdeadbeef";
+    IERC20 token;
 
-//     function setUp() public {
-//         counter = new Counter();
-//         counter.setNumber(0);
-//     }
+    event PaymentReceived(address sender, address token, uint256 amount, bytes barcode);
 
-//     function test_Increment() public {
-//         counter.increment();
-//         assertEq(counter.number(), 1);
-//     }
+    function setUp() public {
+        schnaps = new Schnaps(owner);
+        token = new TestToken(supply);
+        token.transfer(address(this), amount);
+        token.approve(address(schnaps), amount);
+    }
 
-//     function testFuzz_SetNumber(uint256 x) public {
-//         counter.setNumber(x);
-//         assertEq(counter.number(), x);
-//     }
-// }
+    function test_payWithEth() public {
+        vm.expectEmit(true, true, true, true);
+        emit PaymentReceived(address(this), address(0), amount, barcode);
+        schnaps.payWithEth{value: amount}(barcode);
+    }
+
+    function test_payWithToken() public {
+        vm.expectEmit(true, true, true, true);
+        emit PaymentReceived(address(this), address(token), amount, barcode);
+        schnaps.payWithToken(token, amount, barcode);
+    }
+}
