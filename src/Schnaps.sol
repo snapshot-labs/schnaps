8da49518ea5aa734e778725eac5f8d3ba2a8fa06
Orlando <orlando@snapshot.org>
2023-11-14 13:16:09 +0000

    chore: natspec and changed eth->native-token

--- src/Schnaps.sol
+++ src/Schnaps.sol
@@ -1,56 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
+/// @title Schnaps Payment Receiver
+/// @notice A contract that allows the receival of payments via the chain's native token as well as arbitrary ERC20 tokens.
+///         Each payment is referenced by a barcode, which is an arbitrary byte array.
+/// @dev The contract does not perform any checks on the payment validity. This must be done off-chain using the emitted event data.
 contract Schnaps is Ownable {
     using SafeERC20 for IERC20;
 
-    // Initialize `owner`.
+    /// @dev Constructor.
     constructor(address initialOwner) Ownable(initialOwner) {}
 
-    /// Event emitted when a payment is received. `token` will be set to 0 for ETH.
-    // TODO: decide what to put in barcode
+    /// @notice Emitted when a payment is received.
+    /// @param  sender The address of the sender.
+    /// @param  token The address of the token used for payment.
+    ///               It will be set to the zero address if the native token of the chain is used.
+    /// @param  amount The amount of the token used for payment.
+    /// @param  barcode The barcode of the payment.
     event PaymentReceived(address sender, address token, uint256 amount, bytes barcode);
 
-    // Event emitted when a withdrawal occurs. `token` will be set to 0 for ETH.
-    event Withdrawal(address receiver, address token, uint256 amount);
+    /// @notice Emitted when a withdrawal occurs.
+    /// @param  recipient The address of the recipient of the withdrawal.
+    /// @param  token The address of the token withdrawn.
+    ///               It will be set to the zero address if the native token of the chain is used.
+    /// @param  amount The amount of the token withdrawn.
+    event Withdrawal(address recipient, address token, uint256 amount);
 
-    /// @notice Proceed to a payment using ETH.
-    ///
+    /// @notice Makes a payment using the native token of the chain.
     /// @param barcode The barcode of the payment.
-    function payWithEth(bytes calldata barcode) external payable {
+    function payWithNativeToken(bytes calldata barcode) external payable {
         emit PaymentReceived(msg.sender, address(0), msg.value, barcode);
     }
 
-    /// @notice Proceed to a payment using ERC20 token.
-    /// @dev Any token is accepted here, but in practice only tokens accepted by the
-    ///      frontend will be *accepted*.
-    /// @param token The address of the ERC20 token.
-    /// @param amount The amount of the ERC20 token.
+    /// @notice Makes a payment using an ERC20 token.
+    /// @param token The address of the token.
+    /// @param amount The amount of the token.
     /// @param barcode The barcode of the payment.
-    function payWithToken(IERC20 token, uint256 amount, bytes calldata barcode) external {
+    function payWithERC20Token(IERC20 token, uint256 amount, bytes calldata barcode) external {
         token.safeTransferFrom(msg.sender, address(this), amount);
         emit PaymentReceived(msg.sender, address(token), amount, barcode);
     }
 
-    /// @notice Withdraw ETH from the contract.
-    ///
-    /// @param to The address to send ETH to.
-    /// @param amount The amount of ETH to send.
-    function withdrawEth(address payable to, uint256 amount) external onlyOwner {
+    /// @notice Withdraws native tokens from the contract.
+    /// @param recipient The address of the recipient of the withdrawal.
+    /// @param amount The amount of the native token to withdraw.
+    function withdrawNativeToken(address payable recipient, uint256 amount) external onlyOwner {
         to.transfer(amount);
         emit Withdrawal(to, address(0), amount);
     }
 
-    /// @notice Withdraw ERC20 token from the contract.
-    ///
-    /// @param token The address of the ERC20 token.
-    /// @param to The address to send ERC20 token to.
-    /// @param amount The amount of ERC20 token to send.
-    function withdrawToken(IERC20 token, address to, uint256 amount) external onlyOwner {
+    /// @notice Withdraws ERC20 tokens from the contract.
+    /// @param token The address of the token.
+    /// @param recipient The address of the recipient of the withdrawal.
+    /// @param amount The amount of the token to withdraw.
+    function withdrawERC20Token(IERC20 token, address recipient, uint256 amount) external onlyOwner {
         token.safeTransfer(to, amount);
         emit Withdrawal(to, address(token), amount);
     }
